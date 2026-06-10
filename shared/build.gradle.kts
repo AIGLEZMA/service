d422111f891fffc3232c8a8cec27ee23dc7a36f3
AIGLEZMA <aiglezdev@gmail.com>
2026-06-10 18:25:07 +0100

    chore: Add navigation and serialization dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.sqldelight)
-    //alias(libs.plugins.kotlinxSerialization)
+    alias(libs.plugins.kotlinxSerialization)
 }
 
 kotlin {
@@ -14,12 +14,15 @@ kotlin {
         commonMain.dependencies {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
+            implementation(compose.materialIconsExtended)
             implementation(libs.compose.material3)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+            implementation(libs.jetbrains.navigation3.ui)
+            implementation(libs.kotlinx.serialization.core)
             implementation(libs.runtime)
         }
         commonTest.dependencies {
